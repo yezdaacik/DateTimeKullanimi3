790fd504f662812bfcf3715acf005615fbe2d56b
Yezda <145451081+yezdanuracik@users.noreply.github.com>
2024-01-02 10:01:43 +0300

    Add files via upload

---
+++ DateTime Kullanımı 3 815/DateTime Kullanımı 3 815/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DateTime_Kullanımı_3_815
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblTarih = new System.Windows.Forms.Label();
+            this.lblOdemeGunu = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblSonOdeme = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(42, 31);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(90, 38);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblTarih
+            // 
+            this.lblTarih.AutoSize = true;
+            this.lblTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTarih.Location = new System.Drawing.Point(39, 112);
+            this.lblTarih.Name = "lblTarih";
+            this.lblTarih.Size = new System.Drawing.Size(44, 20);
+            this.lblTarih.TabIndex = 1;
+            this.lblTarih.Text = "Tarih";
+            // 
+            // lblOdemeGunu
+            // 
+            this.lblOdemeGunu.AutoSize = true;
+            this.lblOdemeGunu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOdemeGunu.Location = new System.Drawing.Point(39, 142);
+            this.lblOdemeGunu.Name = "lblOdemeGunu";
+            this.lblOdemeGunu.Size = new System.Drawing.Size(105, 20);
+            this.lblOdemeGunu.TabIndex = 2;
+            this.lblOdemeGunu.Text = "Ödeme Günü";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(150, 83);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // lblSonOdeme
+            // 
+            this.lblSonOdeme.AutoSize = true;
+            this.lblSonOdeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonOdeme.Location = new System.Drawing.Point(39, 83);
+            this.lblSonOdeme.Name = "lblSonOdeme";
+            this.lblSonOdeme.Size = new System.Drawing.Size(94, 20);
+            this.lblSonOdeme.TabIndex = 4;
+            this.lblSonOdeme.Text = "Son Ödeme";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSonOdeme);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblOdemeGunu);
+            this.Controls.Add(this.lblTarih);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblTarih;
+        private System.Windows.Forms.Label lblOdemeGunu;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblSonOdeme;
+    }
+}
+
---
+++ DateTime Kullanımı 3 815/DateTime Kullanımı 3 815/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTime_Kullanımı_3_815
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            DateTime bugun = DateTime.Today;
+
+            DateTime odemeGunu = dateTimePicker1.Value;
+
+
+            lblTarih.Text = "Tarih: " + bugun.ToShortDateString();
+            lblOdemeGunu.Text = "Ödeme Günü: " + odemeGunu.ToShortDateString();
+        }
+    }
+}
